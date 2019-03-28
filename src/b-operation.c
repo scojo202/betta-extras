12540a13215e6ee2fdea6e125f994e03c16835dc
Scott O. Johnson <scojo202@gmail.com>
2019-03-28 19:55:26 -0400

    cleanup BOperation 	modified:   src/b-operation.c

--- src/b-operation.c
+++ src/b-operation.c
@@ -43,44 +43,44 @@ static void b_operation_class_init(BOperationClass * klass)
 }
 
 double *b_create_input_array_from_vector(BVector * input, gboolean is_new,
-					 unsigned int old_size,
-					 double *old_input)
+                                         unsigned int old_size,
+                                         double *old_input)
 {
-	double *d = old_input;
-	unsigned int size = b_vector_get_len(input);
-	if (!is_new) {
-		if (old_size != size) {
-			g_free(old_input);
-			d = g_new(double, size);
-		}
-	} else {
-		d = g_new(double, size);
-	}
-	memcpy(d, b_vector_get_values(input), size * sizeof(double));
-	return d;
+  double *d = old_input;
+  unsigned int size = b_vector_get_len(input);
+  if (!is_new) {
+    if (old_size != size) {
+      g_clear_pointer(&old_input,g_free);
+      d = g_try_new(double, size);
+    }
+  } else {
+    d = g_try_new(double, size);
+  }
+  memcpy(d, b_vector_get_values(input), size * sizeof(double));
+  return d;
 }
 
 double *b_create_input_array_from_matrix(BMatrix * input, gboolean is_new,
-					 BMatrixSize old_size,
-					 double *old_input)
+                                         BMatrixSize old_size,
+                                         double *old_input)
 {
-	double *d = old_input;
-	unsigned int old_nrow = old_size.rows;
-	unsigned int old_ncol = old_size.columns;
-	BMatrixSize size = b_matrix_get_size(input);
-	if(size.rows<1 || size.columns<1) {
-		return NULL;
-	}
-	if (!is_new) {
-		if (old_nrow != size.rows || old_ncol != size.columns) {
-			g_free(old_input);
-			d = g_new(double, size.rows * size.columns);
-		}
-	} else {
-		d = g_new(double, size.rows * size.columns);
-	}
+  double *d = old_input;
+  unsigned int old_nrow = old_size.rows;
+  unsigned int old_ncol = old_size.columns;
+  BMatrixSize size = b_matrix_get_size(input);
+  if(size.rows<1 || size.columns<1) {
+    return NULL;
+  }
+  if (!is_new) {
+    if (old_nrow != size.rows || old_ncol != size.columns) {
+      g_clear_pointer(&old_input,g_free);
+      d = g_try_new(double, size.rows * size.columns);
+    }
+  } else {
+    d = g_try_new(double, size.rows * size.columns);
+  }
 	if(d==NULL) {
-		d = g_new(double, size.rows * size.columns);
+		d = g_try_new(double, size.rows * size.columns);
 	}
 	g_assert(b_matrix_get_values(input));
 	memcpy(d, b_matrix_get_values(input),
@@ -139,23 +139,22 @@ BData *b_data_new_from_operation(BOperation *op, BData *input)
  *
  **/
 GTask *b_operation_get_task(BOperation * op, gpointer user_data,
-			    GAsyncReadyCallback cb, gpointer cb_data)
+                            GAsyncReadyCallback cb, gpointer cb_data)
 {
-	GTask *task = g_task_new(op, NULL, cb, cb_data);
+  GTask *task = g_task_new(op, NULL, cb, cb_data);
 
-	g_task_set_task_data(task, user_data, NULL);
-	return task;
+  g_task_set_task_data(task, user_data, NULL);
+  return task;
 }
 
 static void
-task_thread_func(GTask * task,
-		 gpointer source_object,
-		 gpointer task_data, GCancellable * cancellable)
+task_thread_func(GTask * task, gpointer source_object,
+                 gpointer task_data, GCancellable * cancellable)
 {
-	BOperation *op = (BOperation *) source_object;
-	BOperationClass *klass = B_OPERATION_GET_CLASS(op);
-	gpointer output = klass->op_func(task_data);
-	g_task_return_pointer(task, output, NULL);
+  BOperation *op = (BOperation *) source_object;
+  BOperationClass *klass = B_OPERATION_GET_CLASS(op);
+  gpointer output = klass->op_func(task_data);
+  g_task_return_pointer(task, output, NULL);
 }
 
 /**
@@ -169,11 +168,11 @@ task_thread_func(GTask * task,
  *
  **/
 void b_operation_run_task(BOperation * op, gpointer user_data,
-			  GAsyncReadyCallback cb, gpointer cb_data)
+                          GAsyncReadyCallback cb, gpointer cb_data)
 {
-	GTask *task = b_operation_get_task(op, user_data, cb, cb_data);
-	g_task_run_in_thread(task, task_thread_func);
-	g_object_unref(task);
+  GTask *task = b_operation_get_task(op, user_data, cb, cb_data);
+  g_task_run_in_thread(task, task_thread_func);
+  g_object_unref(task);
 }
 
 /**
@@ -187,8 +186,8 @@ void b_operation_run_task(BOperation * op, gpointer user_data,
  **/
 gpointer b_operation_create_task_data(BOperation * op, BData * input)
 {
-	BOperationClass *klass = B_OPERATION_GET_CLASS(op);
-	return klass->op_data(op, NULL, input);
+  BOperationClass *klass = B_OPERATION_GET_CLASS(op);
+  return klass->op_data(op, NULL, input);
 }
 
 /**
@@ -200,8 +199,8 @@ gpointer b_operation_create_task_data(BOperation * op, BData * input)
  * Update an existing task data structure, possibly for a new input object.
  **/
 void b_operation_update_task_data(BOperation * op, gpointer task_data,
-				  BData * input)
+                                  BData * input)
 {
-	BOperationClass *klass = B_OPERATION_GET_CLASS(op);
-	klass->op_data(op, task_data, input);
+  BOperationClass *klass = B_OPERATION_GET_CLASS(op);
+  klass->op_data(op, task_data, input);
 }
